e54ad074e6a2de1722c7be01f66fe2db8922556e
matvey <mot.112511.pog@gmail.com>
2024-10-27 11:54:04 +0200

    Initial Commit

---
+++ todoList/todoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoListApp.swift
+//  todoList
+//
+//  Created by Матвей Погодаев on 27.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct todoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
